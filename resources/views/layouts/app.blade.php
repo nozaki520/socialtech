64daddb9dafa5b8b129157eab065131ec711fb93
nozaki520 <mikuhaorenoyome3939@gmail.com>
2023-05-13 22:51:42 +0900

    ララベルツイッター

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,26 +10,18 @@
 </head>
 
 <body style="padding: 60px 0;">
-    <header>
-        <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">            
-            <div class="container">                               
-                <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
-            </div>
-        </nav>
-    </header>
-    
-    <main>
-        <article>
-            <div class="container">
-                <h1 class="fs-2 my-3">@yield('title')</h1>
-                @yield('content')
-            </div>
-        </article>
-    </main>
+    @include('layouts.header')
 
-    <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
-        <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
-    </footer>
+   <main>
+       <article>
+           <div class="container">
+               <h1 class="fs-2 my-3">@yield('title')</h1>
+               @yield('content')
+           </div>
+       </article>
+   </main>
+
+    @include('layouts.footer')
 </body>
 
 </html>
